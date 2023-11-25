676c48ea9dd19334c786e20aa12a3c5f7dc705cd
Gabriel <gabriellagrota23@gmail.com>
2023-11-25 18:20:34 -0300

    Refac: - Transactional removido - Proparação de exception JsonProcessingException adicionada

--- src/main/java/br/akd/svc/akadia/modules/web/clientesistema/services/crud/ClienteSistemaService.java
+++ src/main/java/br/akd/svc/akadia/modules/web/clientesistema/services/crud/ClienteSistemaService.java
@@ -3,18 +3,16 @@ package br.akd.svc.akadia.modules.web.clientesistema.services.crud;
 import br.akd.svc.akadia.modules.web.clientesistema.models.dto.request.atualizacao.AtualizaClienteSistemaRequest;
 import br.akd.svc.akadia.modules.web.clientesistema.models.dto.request.criacao.ClienteSistemaRequest;
 import br.akd.svc.akadia.modules.web.clientesistema.models.dto.response.ClienteSistemaResponse;
-import org.springframework.transaction.annotation.Transactional;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.UUID;
 
 public interface ClienteSistemaService {
 
-    @Transactional
-    ClienteSistemaResponse cadastraNovoCliente(ClienteSistemaRequest clienteSistemaRequest);
+    ClienteSistemaResponse cadastraNovoCliente(ClienteSistemaRequest clienteSistemaRequest) throws JsonProcessingException;
 
-    @Transactional
     ClienteSistemaResponse atualizaDadosCliente(UUID uuidClienteSistema,
-                                                AtualizaClienteSistemaRequest atualizaClienteSistemaRequest);
+                                                AtualizaClienteSistemaRequest atualizaClienteSistemaRequest) throws JsonProcessingException;
 
 
 }
