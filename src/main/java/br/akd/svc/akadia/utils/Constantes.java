ddc59eaa98b1f9fb5dc13a28745d2eb2959c30da
Gabriel <gabriellagrota23@gmail.com>
2023-11-27 14:35:46 -0300

    Feat: - Novas constantes REGEX_CARACTERES_ESPECIAIS_CPFCNPJ e CONVERSAO_SUCESSO criadas

--- src/main/java/br/akd/svc/akadia/utils/Constantes.java
+++ src/main/java/br/akd/svc/akadia/utils/Constantes.java
@@ -44,4 +44,8 @@ public class Constantes {
 
     public static final String ERRO_INTERNO = "Ocorreu um erro interno no servidor da aplicação. Favor " +
             "entrar com contato com o suporte";
+
+    public static final String CONVERSAO_SUCESSO = "Conversão de tipagem realizada com sucesso";
+
+    public static final String REGEX_CARACTERES_ESPECIAIS_CPFCNPJ = "(\\.?-?/?)";
 }
