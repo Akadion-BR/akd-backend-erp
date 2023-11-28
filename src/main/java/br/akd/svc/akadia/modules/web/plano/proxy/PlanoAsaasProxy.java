789249806f778048a512615bca85b74afe2fa36d
Gabriel <gabriellagrota23@gmail.com>
2023-11-28 10:50:40 -0300

    Refac: - Operação de consulta de plano de assinatura à integradora ASAAS removida, pois não estava sendo utilizada

--- src/main/java/br/akd/svc/akadia/modules/web/plano/proxy/PlanoAsaasProxy.java
+++ src/main/java/br/akd/svc/akadia/modules/web/plano/proxy/PlanoAsaasProxy.java
@@ -2,7 +2,6 @@ package br.akd.svc.akadia.modules.web.plano.proxy;
 
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.atualizacao.models.request.AtualizaAssinaturaAsaasRequest;
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.atualizacao.models.response.AtualizaAssinaturaAsaasResponse;
-import br.akd.svc.akadia.modules.web.plano.proxy.operations.consulta.response.ConsultaAssinaturaResponse;
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.criacao.models.request.CriaPlanoAsaasRequest;
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.criacao.models.response.CriaPlanoAsaasResponse;
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.remocao.response.CancelamentoAssinaturaResponse;
@@ -14,10 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public interface PlanoAsaasProxy {
     @PostMapping(value = "/subscriptions")
     ResponseEntity<CriaPlanoAsaasResponse> cadastraNovaAssinatura(@RequestBody CriaPlanoAsaasRequest criaPlanoAsaasRequest,
-                                                                  @RequestHeader(value = "access_token") String accessToken);
-
-    @GetMapping(value = "/subscriptions/{idAssinatura}")
-    ResponseEntity<ConsultaAssinaturaResponse> consultaAssinatura(@PathVariable String idAssinatura,
                                                                   @RequestHeader(value = "access_token") String accessToken);
 
     @PostMapping(value = "/subscriptions/{idAssinatura}")
