f1da8027bb690dcb7db3f5ce5a44595d274b56cc
Gabriel <gabriellagrota23@gmail.com>
2023-11-28 10:43:19 -0300

    Feat: - Validação dos retornos do Feign client criada para atualização de plano na integradora asaas

--- src/main/java/br/akd/svc/akadia/modules/web/plano/proxy/operations/atualizacao/impl/AtualizacaoPlanoAsaasProxyImpl.java
+++ src/main/java/br/akd/svc/akadia/modules/web/plano/proxy/operations/atualizacao/impl/AtualizacaoPlanoAsaasProxyImpl.java
@@ -1,13 +1,14 @@
 package br.akd.svc.akadia.modules.web.plano.proxy.operations.atualizacao.impl;
 
-import br.akd.svc.akadia.exceptions.InvalidRequestException;
+import br.akd.svc.akadia.exceptions.InternalErrorException;
 import br.akd.svc.akadia.modules.web.plano.models.entity.PlanoEntity;
 import br.akd.svc.akadia.modules.web.plano.proxy.PlanoAsaasProxy;
-import br.akd.svc.akadia.modules.web.plano.proxy.global.request.enums.CycleEnum;
+import br.akd.svc.akadia.modules.web.plano.proxy.operations.atualizacao.impl.utils.AtualizacaoPlanoAsaasProxyUtils;
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.atualizacao.models.request.AtualizaAssinaturaAsaasRequest;
 import br.akd.svc.akadia.modules.web.plano.proxy.operations.atualizacao.models.response.AtualizaAssinaturaAsaasResponse;
-import br.akd.svc.akadia.modules.web.plano.utils.ConstantesPlano;
 import br.akd.svc.akadia.utils.Constantes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,59 +21,40 @@ public class AtualizacaoPlanoAsaasProxyImpl {
     @Autowired
     PlanoAsaasProxy planoAsaasProxy;
 
-    public void realizaAtualizacaoDePlanoDeAssinaturaNaIntegradoraAsaas(PlanoEntity planoAtualizado) {
+    @Autowired
+    AtualizacaoPlanoAsaasProxyUtils atualizacaoPlanoAsaasProxyUtils;
+
+    public void realizaAtualizacaoDePlanoDeAssinaturaNaIntegradoraAsaas(PlanoEntity planoAtualizado) throws JsonProcessingException {
 
         log.debug("Método de serviço responsável pela atualização de assinatura na integradora ASAAS acessado");
 
-        log.debug("Iniciando construção do objeto AtualizaAssinaturaAsaasRequest...");
-        AtualizaAssinaturaAsaasRequest atualizaAssinaturaAsaasRequest = AtualizaAssinaturaAsaasRequest.builder()
-                .billingType(planoAtualizado.getFormaPagamentoSistemaEnum())
-                .value(planoAtualizado.getTipoPlanoEnum().getValor())
-                .nextDueDate(planoAtualizado.getDataVencimento())
-                .discount(null)
-                .interest(null)
-                .fine(null)
-                .cycle(CycleEnum.MONTHLY)
-                .description(planoAtualizado.getTipoPlanoEnum().getDesc())
-                .updatePendingPayments(true)
-                .externalReference(null)
-                .build();
-
-        ResponseEntity<AtualizaAssinaturaAsaasResponse> responseAsaas;
+        log.info("Iniciando construção do objeto AtualizaAssinaturaAsaasRequest...");
+        AtualizaAssinaturaAsaasRequest atualizaAssinaturaAsaasRequest =
+                new AtualizaAssinaturaAsaasRequest().buildFromRequest(planoAtualizado);
+        log.info("Objeto AtualizaAssinaturaAsaasRequest construído com sucesso");
 
         try {
             log.debug("Realizando envio de requisição de atualização de assinatura para a integradora ASAAS...");
-            responseAsaas =
+            ResponseEntity<AtualizaAssinaturaAsaasResponse> responseAsaas =
                     planoAsaasProxy.atualizaAssinatura(
                             planoAtualizado.getCodigoAssinaturaAsaas(),
                             atualizaAssinaturaAsaasRequest,
                             System.getenv(Constantes.TOKEN_ASAAS));
+
+            log.info("Realizando validações referente à resposta da integradora...");
+            atualizacaoPlanoAsaasProxyUtils.realizaValidacaoResponseAsaas(responseAsaas);
+            log.info("Validações realizadas com sucesso. Plano atualizado na ASAAS com sucesso.");
+        } catch (FeignException feignException) {
+            log.error("Ocorreu um erro durante a integração com o ASAAS para atualização de assinatura: {}",
+                    feignException.getMessage());
+
+            log.info("Iniciando acesso ao método responsável por realizar o tratamento do erro retornado pelo " +
+                    "feign client...");
+            throw atualizacaoPlanoAsaasProxyUtils.realizaTratamentoRetornoErroFeignException(feignException);
         } catch (Exception e) {
-            log.error(ConstantesPlano.ERRO_ATUALIZACAO_ASSINATURA_ASAAS
-                    + e.getMessage());
-            throw new InvalidRequestException(ConstantesPlano.ERRO_ATUALIZACAO_ASSINATURA_ASAAS
-                    + e.getMessage());
+            log.error("Ocorreu um erro interno durante a atualização do plano na integradora de " +
+                    "pagamentos ASAAS: {}", e.getMessage());
+            throw new InternalErrorException(Constantes.ERRO_INTERNO);
         }
-
-        if (responseAsaas == null) {
-            log.error("O valor retornado pela integradora na atualização da assinatura é nulo");
-            throw new InvalidRequestException(Constantes.RETORNO_INTEGRADORA_NULO);
-        }
-
-        if (responseAsaas.getStatusCodeValue() != 200) {
-            log.error("Ocorreu um erro no processo de atualização da assinatura na integradora de pagamentos: {}",
-                    responseAsaas.getBody());
-            throw new InvalidRequestException(ConstantesPlano.ERRO_CRIACAO_ASSINATURA_ASAAS
-                    + responseAsaas.getBody());
-        }
-        log.debug("Atualização de assinatura ASAAS realizada com sucesso");
-
-        AtualizaAssinaturaAsaasResponse planoAsaasResponse = responseAsaas.getBody();
-
-        if (planoAsaasResponse == null) {
-            log.error("O valor retornado pela integradora na atualização da assinatura é nulo");
-            throw new InvalidRequestException(Constantes.RETORNO_INTEGRADORA_NULO);
-        }
-
     }
 }
