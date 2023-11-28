48d92e7340eabe2272f32fabee486465d5b9f5c9
Gabriel <gabriellagrota23@gmail.com>
2023-11-28 10:47:03 -0300

    Feat: - Criado método Cron que é disparado todos os dias uma hora da manhã que tem como objetido buscar os clientes sistemicos que possuem planos que possuem a data de agendamento de remoção de plano para a data atual e setar o status de seus planos como inativo

--- src/main/java/br/akd/svc/akadia/modules/web/clientesistema/services/cron/ClienteSistemaCronService.java
+++ src/main/java/br/akd/svc/akadia/modules/web/clientesistema/services/cron/ClienteSistemaCronService.java
@@ -26,6 +26,8 @@ public class ClienteSistemaCronService {
     @Autowired
     ClienteSistemaRepositoryImpl clienteSistemaRepositoryImpl;
 
+    //TODO CRIAR MÉTODO AVISANDO CLIENTES QUE IRÁ VENCER EM TRÊS DIAS
+
     @Scheduled(cron = "0 0 2 * * *", zone = "America/Sao_Paulo")
     public void atualizaStatusDePlanosComPagamentoVencido() {
         log.info("Método de serviço responsável por realizar a atualização de status de planos com " +
@@ -47,6 +49,32 @@ public class ClienteSistemaCronService {
         //TODO VALIDAR SE PERSISTÊNCIA ESTÁ FUNCIONANDO
         log.info("Iniciando persistência da lista de clientes com status de planos atualizados no banco de dados...");
         clienteSistemaRepositoryImpl.implementaPersistenciaEmMassa(clientesComPagamentoAtrasado);
+        log.info("Persistência realizada com sucesso");
+    }
+
+    @Scheduled(cron = "0 0 1 * * *", zone = "America/Sao_Paulo")
+    public void atualizaStatusDePlanosComAgendamentoRemocaoVencido() {
+        log.info("Método de serviço responsável por realizar a atualização de status de planos agendamento " +
+                "de remoção para hoje");
+
+        //TODO IMPLEMENTAR ENVIO ASSINCRONO DE E-MAILS PARA PLANOS VENCIDOS
+        log.info("Iniciando acesso à implementação do repositório de busca de clientes sistêmicos com " +
+                "remoção agendada vencida...");
+        List<ClienteSistemaEntity> clientesComRemocaoAgendadaVencida =
+                clienteSistemaRepositoryImpl.implementaBuscaPorPlanosComCancelamentoAgendadoVencido();
+        log.info("Clientes com remoção agendada vencida obtidos");
+
+        log.info("Iniciando iteração pela listagem de clientes sistêmicos...");
+        for (ClienteSistemaEntity clienteSistema : clientesComRemocaoAgendadaVencida) {
+            log.info("Setando plano do cliente sistêmico de id {} como INATIVO...", clienteSistema.getId());
+            clienteSistema.getPlano().setStatusPlanoEnum(StatusPlanoEnum.INATIVO);
+            log.info("Atualização de status do plano do cliente sistêmico realizada com sucesso");
+        }
+
+        //TODO VALIDAR SE PERSISTÊNCIA ESTÁ FUNCIONANDO
+        log.info("Iniciando persistência da lista de clientes sistêmicos com status de planos atualizados no " +
+                "banco de dados...");
+        clienteSistemaRepositoryImpl.implementaPersistenciaEmMassa(clientesComRemocaoAgendadaVencida);
         log.info("Persistência realizada com sucesso");
     }
 }
