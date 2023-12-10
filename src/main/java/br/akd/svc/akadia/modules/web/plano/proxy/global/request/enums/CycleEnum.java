66eb61e7cc03d7507a06691413e750462b3a9009
Gabriel <gabriellagrota23@gmail.com>
2023-12-10 11:02:13 -0300

    refac: - Módulo WEB movido para microserviço apartado

--- src/main/java/br/akd/svc/akadia/modules/web/plano/proxy/global/request/enums/CycleEnum.java
+++
@@ -1,5 +0,0 @@
-package br.akd.svc.akadia.modules.web.plano.proxy.global.request.enums;
-
-public enum CycleEnum {
-    WEEKLY, BIWEEKLY, MONTHLY, QUARTERLY, SEMIANNUALY, YEARLY
-}
--- src/main/java/br/akd/svc/akadia/modules/web/plano/proxy/operations/criacao/models/response/enums/StatusEnum.java
+++
@@ -1,5 +0,0 @@
-package br.akd.svc.akadia.modules.web.plano.proxy.operations.criacao.models.response.enums;
-
-public enum StatusEnum {
-    ACTIVE, EXPIRED
-}
