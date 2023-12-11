abb920f1ca29d913884b6ed0202aa23a89820eca
Gabriel Lagrota <gabriellagrota23@gmail.com>
2023-12-11 20:30:20 -0300

    remove: - Módulo de leads removido para implementação futura

--- src/main/java/br/akd/svc/akadia/modules/backoffice/lead/repository/LeadRepository.java
+++
@@ -1,8 +0,0 @@
-package br.akd.svc.akadia.modules.backoffice.lead.repository;
-
-import br.akd.svc.akadia.modules.backoffice.lead.models.entity.LeadEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeadRepository extends JpaRepository<LeadEntity, Long> {}
